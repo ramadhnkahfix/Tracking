0f6d916e4b05032a62c8ab2cd57fcf04c2c35aa6
Ramadhani151811513044 <ramadhani.alief04@gmail.com>
2021-03-25 08:58:36 +0700

    progress

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,105 +1,89 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="keywords" content="login">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Login</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Bea Cukai Kediri | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Start css -->
-    <link href="{{ asset('/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('/assets/css/style.css') }}" rel="stylesheet" type="text/css">
-    <!-- FontAwesome css -->
-    <link rel="stylesheet" href="{{ asset('/assets/fontawesome/css/all.min.css') }}">
-    <style>
-        .card{
-            box-shadow: 10px 10px 15px;
-        }
-
-        .form-control {
-            border: 0;
-        }
-
-        .input-group {
-            border: 0.5px solid grey;
-        }
-
-        .input-group-text {
-            background-color: #E9ECEF;
-        }
-
-        .fas {
-            color: black;
-        }
-
-        .btn:hover {
-            background-color: rgb(19, 75, 49);
-        }
-    </style>
-    <!-- End css -->
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body class="vertical-layout">
-    <!-- Start Containerbar -->
-    <div id="containerbar" class="containerbar">
-        <!-- Start Container -->
-        <div class="container">
-            <div class="auth-box login-box">
-                <!-- Start row -->
-                <div class="row no-gutters align-items-center justify-content-center">
-                    <!-- Start col -->
-                    <div class="col-md-6 col-lg-5">
-                        <!-- Start Auth Box -->
-                        <div class="auth-box-right">
-                            <div class="card">
-                                <div class="card-body">
-                                    <form action="" method="POST">
-                                    @csrf
-                                        <div class="form-head">
-                                            <h1>Tracking</h1>
-                                        </div>                                        
-                                        <h4 class="text-primary my-4">Log in !</h4>
-                                        <div class="form-group input-group">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text">
-                                                    <i class="fas fa-user"></i>
-                                                </span>
-                                            </div>
-                                            <input type="text" class="form-control" name="email" id="email" placeholder="Enter Email here" required>
-                                        </div>
-                                        <div class="form-group input-group">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text">
-                                                    <i class="fas fa-key"></i>
-                                                </span>
-                                            </div>
-                                            <input type="password" class="form-control" name="password" id="password" placeholder="Enter Password here" required>
-                                        </div>
-                                                             
-                                      <button type="submit" class="btn btn-success btn-lg btn-block font-18">Log in</button>
-                                    </form>
-                                    <hr>
-                                    <button type="button" class="btn btn-primary btn-lg btn-block font-18">Register</button>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Auth Box -->
-                    </div>
-                    <!-- End col -->
-                </div>
-                <!-- End row -->
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="#"><img src="{{ url('img/logo_bea_cukai.png') }}" width="50" alt=""> <b>BEA CUKAI KEDIRI</b></a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form action="" method="post">
+        @csrf
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
             </div>
+          </div>
+          @error('email')
+          <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+          </span>
+      @enderror
         </div>
-        <!-- End Container -->
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+          @error('password')
+          <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+          </span>
+      @enderror
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+              <label for="remember">
+                {{ __('Remember Me') }}
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">  {{ __('Login') }}</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
     </div>
-    <!-- End Containerbar -->
-    <!-- Start js -->        
-    <script src="{{ asset('/assets/js/jquery.min.js') }}"></script>
-    {{-- <script src="{{ asset('/assets/js/popper.min.js') }}"></script> --}}
-    <script src="{{ asset('/assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('/assets/js/modernizr.min.js') }}"></script>
-    <script src="{{ asset('/assets/js/detect.js') }}"></script>
-    {{-- <script src="{{ asset('/assets/js/jquery.slimscroll.js') }}"></script> --}}
-    <!-- End js -->
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+
 </body>
 </html>
